4cc2aab7624ac47c79eb349868f50a604c437ca6
jjye6 <jjy00110@gmail.com>
2025-03-23 18:08:34 +0100

    change2

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ var texto2 = document.getElementById("texto2");
 var changeTextButton = document.getElementById("texto_cambiar");
 var restoreTextButton = document.getElementById("texto_recuperar");
 var originalTexts = [texto1.innerHTML,texto2.innerHTML,];
-var newText = "<li>Tengo una profunda pasión por la informática y la tecnología.</li><li>Mi objetivo es crear soluciones innovadoras que optimicen la eficiencia y la seguridad.</li><li>Valoro la integridad, la colaboración y la mejora continua.<li>Creo en el poder de la tecnología para facilitar la comunicación y el acceso a la información.</li><li>Estoy comprometido con el aprendizaje constante y la adaptación a los cambios tecnológicos.</li><li>Mi meta es ofrecer servicios de alta calidad y contribuir al crecimiento de cualquier organización.</li>";
+var newText = "<li>Tengo una profunda pasión por la informática y la tecnología.</li><br><li>Mi objetivo es crear soluciones innovadoras que optimicen la eficiencia y la seguridad.</li><br><li>Valoro la integridad, la colaboración y la mejora continua.</li><br><li>Creo en el poder de la tecnología para facilitar la comunicación y el acceso a la información.</li><br><li>Estoy comprometido con el aprendizaje constante y la adaptación a los cambios tecnológicos.</li><br><li>Mi meta es ofrecer servicios de alta calidad y contribuir al crecimiento de cualquier organización.</li>";
 
 changeTextButton.addEventListener("click", function() {
   texto1.innerHTML = newText;
