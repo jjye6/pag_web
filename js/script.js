ecfb87a2f70868742b0d38ba4b5901a679f89349
Jiajun Ye <129900477+jjye6@users.noreply.github.com>
2026-03-18 15:15:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ var texto2 = document.getElementById("texto2");
 var changeTextButton = document.getElementById("texto_cambiar");
 var restoreTextButton = document.getElementById("texto_recuperar");
 var originalTexts = [texto1.innerHTML,texto2.innerHTML,];
-var newText = "<li>I have a deep passion for computer science and technology.</li><br><li>My objective is to create innovative solutions that optimize efficiency and safety.</li><br><li>I value integrity, collaboration, and continuous improvement.</li><br><li>I believe in the power of technology to facilitate communication and access to information.</li><br><li>I am committed to continuous learning and adapting to technological changes..</li><br><li>My final goal is to offer high-quality services and contribute to the growth of any organization..</li>";
+var newText = "<li>I have a deep passion for computer science and technology.</li><br><li>My objective is to create innovative solutions that optimize efficiency and safety.</li><br><li>I value integrity, collaboration, and continuous improvement.</li><br><li>I believe in the power of technology to facilitate communication and access to information.</li><br><li>I am committed to continuous learning and adapting to technological changes.</li><br><li>My final goal is to offer high-quality services and contribute to the growth of any organization.</li>";
 
 changeTextButton.addEventListener("click", function() {
   texto1.innerHTML = newText;
